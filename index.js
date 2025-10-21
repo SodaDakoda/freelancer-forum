a3dab14fb1b72956df87b34008cf9f85c1eff75d
Mackenzie Green <macgreen813@outlook.com>
2025-10-21 08:08:41 -0700

    created makePerson function

--- index.js
+++ index.js
@@ -102,3 +102,13 @@ const OCCUPATIONS = [
 ];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+
+function makePerson() {
+  const name = NAMES[Math.floor(Math.random() * NAMES.length)];
+  const occupation =
+    OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
+  const rate =
+    Math.floor(Math.random() * (PRICE_RANGE.max - PRICE_RANGE.min + 1)) +
+    PRICE_RANGE.min;
+  return { name, occupation, rate };
+}
